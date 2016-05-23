3cb52ad15151a96c02ca3c56a8fce9a3eae5416e
Wataru Kawakami <kobe_kwave@yahoo.co.jp>
2016-05-23 14:51:36 +0900

    C++のチュートリアルソース追加

---
+++ チュートリアル/main.cpp
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: iryolabo
+ *
+ * Created on 2016/05/17, 16:10
+ */
+
+#include <cstdlib>
+#include<iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+    
+    int num = 0;
+    
+    cout << "整数を入力してください．\n";
+    
+    cin >> num;
+    
+    cout << num << "が入力されました．\n";
+    
+   
+    
+    return 0;
+}
+
